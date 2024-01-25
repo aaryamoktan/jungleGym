520bd3b6b11344a692c56d33314476459ca63d35
aarya moktan <aaryamoktan2055@gmail.com>
2024-01-25 07:01:47 -0800

    first commit

--- src/Content/Book/Book.js
+++ src/Content/Book/Book.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import "./book.css"
 import { useLocation } from 'react-router-dom'
 const Book = ({ props }) => {
